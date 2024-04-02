ab195aec3939de5b89285471177fcd7f9a1acd2a
ali.naderkhooy <4ali.naderkhooy@gmail.com>
2024-04-02 05:09:58 +0430

    fix logic of verification

--- Sources/verification.cpp
+++ Sources/verification.cpp
@@ -3,11 +3,20 @@
 
 #include "ui_verification.h"
 
+#include "QMessageBox"
+#include "QValidator"
+
+int randomNumber;
+
 Verification::Verification(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Verification)
 {
     ui->setupUi(this);
+    int randNum = rand()%9000+1000;
+    randomNumber = randNum;
+    ui->codeLine->setValidator(new QIntValidator);
+    QMessageBox::information(this,"Captcha","Your captcha code is " + QString::number(randNum) + " .");
 }
 
 Verification::~Verification()
@@ -17,8 +26,17 @@ Verification::~Verification()
 
 void Verification::on_confirmButton_clicked()
 {
-    Profile *w = new Profile;
-    w->show();
 
-    close();
+    QString codeText = QString::number(randomNumber);
+    if(ui->codeLine->text() != codeText){
+        randomNumber = rand()%9000+1000;
+        QMessageBox::warning(this,"Eror!","The number was incorrect , the new captcha is " + QString::number(randomNumber) + " .");
+        ui->codeLine->setText("");
+    }
+    else {
+        Profile *w = new Profile;
+        w->show();
+
+        close();
+    }
 }
