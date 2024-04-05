2c5a9659ab806202b1359779e601352862a00fc3
ali.naderkhooy <4ali.naderkhooy@gmail.com>
2024-04-06 03:55:13 +0430

    fix logic of profile

--- Sources/profile.cpp
+++ Sources/profile.cpp
@@ -1,6 +1,8 @@
 #include "Headers/profile.h"
 #include "ui_profile.h"
 
+#include <QMessageBox>
+
 Profile::Profile(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Profile)
@@ -17,6 +19,40 @@ Profile::~Profile()
 
 void Profile::on_submitButton_clicked()
 {
+    const QString jobtitle = ui->jobTitleEdit->text();
+    const QString company = ui->companyEdit->text();
+    const QString jobtype = ui->jobTypeEdit->text();
+    const QString university = ui->universityEdit->text();
+
+    if (jobtitle.length() == 0){
+        QMessageBox::warning(
+            this,
+            "JobTitle Length",
+            "Enter Your JobTitle .");
+        return ;
+    }
+    if(company.length() == 0){
+        QMessageBox::warning(
+            this,
+            "Company Length",
+            "Enter Your Company .");
+        return ;
+    }
+    if(jobtype.length() == 0){
+        QMessageBox::warning(
+            this,
+            "JobType Length",
+            "Enter Your JobType .");
+        return ;
+    }
+    if(university.length() == 0){
+        QMessageBox::warning(
+            this,
+            "University Length",
+            "Enter Your University .");
+        return ;
+    }
+
     hide();
     deleteLater();
     parentWidget()->deleteLater();
